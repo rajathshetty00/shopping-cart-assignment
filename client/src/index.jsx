44d6ce7db5741e7c8218f0c37f6ba1cba856f67d
Rajath Shetty <rajathshetty00@gmail.com>
2022-07-08 19:36:07 +0530

    replays progressive web app creation

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,6 +6,7 @@ import { CssBaseline } from '@mui/material';
 import theme from './theme';
 import {store} from './store';
 import { Provider } from 'react-redux'
+import * as serviceWorker from './service-worker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -19,3 +20,8 @@ root.render(
   </Provider>
 );
 
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.register();
